a6db3c2818595189f1e3fc83e5b8f303e5ad1bcc
taylor.wtf <109391572+taylorwtf@users.noreply.github.com>
2023-04-06 14:19:40 -0700

    updates

--- sketch.js
+++ sketch.js
@@ -11,10 +11,13 @@ function setup() {
   for (let i = 0; i < particleCountSlider.value(); i++) {
     particles.push(new Particle());
   }
+
+  colorMode(HSB, 360, 100, 100, 1);
+  noStroke();
 }
 
 function draw() {
-  background(0);
+  background(0, 0.2);
 
   while (particles.length < particleCountSlider.value()) {
     particles.push(new Particle());
@@ -33,13 +36,17 @@ function draw() {
 class Particle {
   constructor() {
     this.pos = createVector(random(width), random(height));
-    this.vel = createVector(random(-1, 1), random(-1, 1));
-    this.size = random(5, 20);
-    this.color = color(0, 255, 0, 200);
+    this.vel = createVector(0, random(0.5, 3));
+    this.size = random(5, 25);
+    this.hue = 120; // Green hue like in the Matrix
+    this.life = 0;
+    this.charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZ<>+-*/";
+    this.opacity = random(0.5, 1);
   }
 
   update() {
     this.vel.setMag(speedSlider.value());
+    this.vel.y *= 1.01; // Increase the speed of particles
     this.pos.add(this.vel);
 
     if (mouseIsPressed) {
@@ -50,14 +57,19 @@ class Particle {
       this.vel.add(dir);
     }
 
+    if (random() < 0.01) {
+      this.vel.x = random(-1, 1); // Random change in direction
+    }
+
     this.size = sizeSlider.value();
+    this.color = color(this.hue, 100, 100, this.opacity);
+    this.life += 0.05;
   }
 
   display() {
-    noStroke();
     fill(this.color);
     textSize(this.size);
-    text(randomizeCheckbox.checked() ? str(floor(random(2))) : "0", this.pos.x, this.pos.y);
+    text(randomizeCheckbox.checked() ? this.charSet.charAt(floor(random(this.charSet.length))) : "0", this.pos.x, this.pos.y);
   }
 
   checkEdges() {
@@ -68,6 +80,7 @@ class Particle {
     }
     if (this.pos.y > height) {
       this.pos.y = 0;
+      this.opacity = random(0.5, 1); // Reset opacity when wrapping around the screen
     } else if (this.pos.y < 0) {
       this.pos.y = height;
     }
